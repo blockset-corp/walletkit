9a99d4882b9aa38f87ce5b569b46324b929e45a7
Ed Gamble <ed@breadwallet.com>
2020-05-13 10:08:47 -0700

    CORE-912: In 'GET /currencies' request, provide 'verified'.

--- WalletKitSwift/WalletKit/common/BRBlockChainDB.swift
+++ WalletKitSwift/WalletKit/common/BRBlockChainDB.swift
@@ -736,8 +736,18 @@ public class BlockChainDB {
         }
     }
 
-    public func getCurrencies (blockchainId: String? = nil, completion: @escaping (Result<[Model.Currency],QueryError>) -> Void) {
-        bdbMakeRequest (path: "currencies", query: blockchainId.map { zip(["blockchain_id"], [$0]) }) {
+    public func getCurrencies (blockchainId: String? = nil, verified: Bool = true, completion: @escaping (Result<[Model.Currency],QueryError>) -> Void) {
+        let queryKeysBase = [
+            blockchainId.map { (_) in "blockchain_id" },
+            "verified"]
+            .compactMap { $0 } // Remove `nil` from blockchainId
+
+        let queryValsBase: [String] = [
+            blockchainId,
+            verified.description]
+            .compactMap { $0 }  // Remove `nil` from blockchainId
+
+        bdbMakeRequest (path: "currencies", query: zip (queryKeysBase, queryValsBase)) {
             (more: URL?, res: Result<[JSON], QueryError>) in
             precondition (nil == more)
             completion (res.flatMap {
