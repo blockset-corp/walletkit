da913305a1a84db12a7809a2cb335c5221dbd614
Ed Gamble <ed@breadwallet.com>
2021-02-02 16:16:55 -0800

    CORE-1102: Update blockchain checkpoints/heights

--- WalletKitCore/src/crypto/BRCryptoConfig.h
+++ WalletKitCore/src/crypto/BRCryptoConfig.h
@@ -35,7 +35,7 @@
 // MARK: - BTC
 
 #define NETWORK_NAME    "Bitcoin"
-DEFINE_NETWORK (btcMainnet,  "bitcoin-mainnet", NETWORK_NAME, "mainnet", true, 612691, 6)
+DEFINE_NETWORK (btcMainnet,  "bitcoin-mainnet", NETWORK_NAME, "mainnet", true, 668821, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-mainnet", "18", "10m", 10 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoin-mainnet",     "bitcoin-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_BTC,  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoin-mainnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
@@ -43,7 +43,7 @@ DEFINE_CURRENCY ("bitcoin-mainnet",     "bitcoin-mainnet:__native__",   NETWORK_
 DEFINE_ADDRESS_SCHEMES  ("bitcoin-mainnet", CRYPTO_ADDRESS_SCHEME_BTC_SEGWIT,   CRYPTO_ADDRESS_SCHEME_BTC_LEGACY)
 DEFINE_MODES            ("bitcoin-mainnet", CRYPTO_SYNC_MODE_API_ONLY,          CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (btcTestnet,  "bitcoin-testnet", NETWORK_NAME, "testnet", false, 1660401, 6)
+DEFINE_NETWORK (btcTestnet,  "bitcoin-testnet", NETWORK_NAME, "testnet", false, 1931985, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-testnet", "18", "10m", 10 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoin-testnet",     "bitcoin-testnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_BTC,  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoin-testnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
@@ -55,7 +55,7 @@ DEFINE_MODES            ("bitcoin-testnet", CRYPTO_SYNC_MODE_API_ONLY,          
 // MARK: - BCH
 
 #define NETWORK_NAME    "Bitcoin Cash"
-DEFINE_NETWORK (bchMainnet,  "bitcoincash-mainnet", NETWORK_NAME, "mainnet", true, 617640, 6)
+DEFINE_NETWORK (bchMainnet,  "bitcoincash-mainnet", NETWORK_NAME, "mainnet", true, 673068, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-mainnet", "2", "10m", 20 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoincash-mainnet",     "bitcoincash-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_BCH,  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoincash-mainnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
@@ -63,7 +63,7 @@ DEFINE_CURRENCY ("bitcoincash-mainnet",     "bitcoincash-mainnet:__native__",   
 DEFINE_ADDRESS_SCHEMES  ("bitcoincash-mainnet", CRYPTO_ADDRESS_SCHEME_BTC_LEGACY)
 DEFINE_MODES            ("bitcoincash-mainnet", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (bchTestnet,  "bitcoincash-testnet", NETWORK_NAME, "testnet", false, 1353306, 6)
+DEFINE_NETWORK (bchTestnet,  "bitcoincash-testnet", NETWORK_NAME, "testnet", false, 1432976, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-testnet", "2", "10m", 20 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoincash-testnet",     "bitcoincash-testnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_BCH,  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoincash-testnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
@@ -75,7 +75,7 @@ DEFINE_MODES            ("bitcoincash-testnet", CRYPTO_SYNC_MODE_API_ONLY,  CRYP
 // MARK: - ETH
 
 #define NETWORK_NAME    "Ethereum"
-DEFINE_NETWORK (ethMainnet,  "ethereum-mainnet", NETWORK_NAME, "mainnet", true, 9273958, 6)
+DEFINE_NETWORK (ethMainnet,  "ethereum-mainnet", NETWORK_NAME, "mainnet", true, 11779945, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-mainnet", "25000000000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ethereum-mainnet",     "ethereum-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_ETH,  "native",   NULL,   true)
     DEFINE_UNIT ("ethereum-mainnet:__native__",      "Wei",         "wei",      0,      "WEI")
@@ -87,7 +87,7 @@ DEFINE_CURRENCY ("ethereum-mainnet",    "ethereum-mainnet:0x558ec3152e2eb2174905
 DEFINE_ADDRESS_SCHEMES  ("ethereum-mainnet", CRYPTO_ADDRESS_SCHEME_ETH_DEFAULT)
 DEFINE_MODES            ("ethereum-mainnet", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_SYNC_MODE_API_WITH_P2P_SEND, CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (ethRopsten,  "ethereum-ropsten", NETWORK_NAME, "testnet", false, 7119019, 6)
+DEFINE_NETWORK (ethRopsten,  "ethereum-ropsten", NETWORK_NAME, "testnet", false, 9588166, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-ropsten", "17500000000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ethereum-ropsten",     "ethereum-ropsten:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_ETH,  "native",   NULL,   true)
     DEFINE_UNIT ("ethereum-ropsten:__native__",      "Wei",         "wei",      0,      "WEI")
@@ -103,7 +103,7 @@ DEFINE_MODES            ("ethereum-ropsten", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_S
 // MARK: XRP
 
 #define NETWORK_NAME    "Ripple"
-DEFINE_NETWORK (xrpMainnet,  "ripple-mainnet", NETWORK_NAME, "mainnet", true, 52713481, 6)
+DEFINE_NETWORK (xrpMainnet,  "ripple-mainnet", NETWORK_NAME, "mainnet", true, 61321875, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ripple-mainnet", "10", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ripple-mainnet",     "ripple-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_XRP,  "native",   NULL,   true)
     DEFINE_UNIT ("ripple-mainnet:__native__",      "Drop",       "drop",      0,      "DROP")
@@ -111,7 +111,7 @@ DEFINE_CURRENCY ("ripple-mainnet",     "ripple-mainnet:__native__",   NETWORK_NA
 DEFINE_ADDRESS_SCHEMES  ("ripple-mainnet", CRYPTO_ADDRESS_SCHEME_GEN_DEFAULT)
 DEFINE_MODES            ("ripple-mainnet", CRYPTO_SYNC_MODE_API_ONLY)
 
-DEFINE_NETWORK (xrpTestnet,  "ripple-testnet", NETWORK_NAME, "testnet", false, 0, 6)
+DEFINE_NETWORK (xrpTestnet,  "ripple-testnet", NETWORK_NAME, "testnet", false, 50000, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ripple-testnet", "10", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ripple-testnet",     "ripple-testnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_XRP,  "native",   NULL,   true)
     DEFINE_UNIT ("ripple-testnet:__native__",      "Drop",       "drop",      0,      "DROP")
@@ -123,7 +123,7 @@ DEFINE_MODES            ("ripple-testnet", CRYPTO_SYNC_MODE_API_ONLY)
 // MARK: HBAR Mainnet
 
 #define NETWORK_NAME    "Hedera"
-DEFINE_NETWORK (hbarMainnet,  "hedera-mainnet", NETWORK_NAME, "mainnet", true, 52473542, 1)
+DEFINE_NETWORK (hbarMainnet,  "hedera-mainnet", NETWORK_NAME, "mainnet", true, 12295580, 1)
 DEFINE_NETWORK_FEE_ESTIMATE ("hedera-mainnet", "500000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("hedera-mainnet",     "hedera-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_HBAR,  "native",   NULL,   true)
     DEFINE_UNIT ("hedera-mainnet:__native__",  "tinybar",     "thbar",  0,  "tℏ")
@@ -143,7 +143,7 @@ DEFINE_MODES            ("hedera-testnet", CRYPTO_SYNC_MODE_API_ONLY)
 // MARK: Tezos Mainnet
 
 #define NETWORK_NAME    "Tezos"
-DEFINE_NETWORK (xtzMainnet,  "tezos-mainnet", NETWORK_NAME, "mainnet", true, 52473542, 1)
+DEFINE_NETWORK (xtzMainnet,  "tezos-mainnet", NETWORK_NAME, "mainnet", true, 1328407, 1)
 DEFINE_NETWORK_FEE_ESTIMATE ("tezos-mainnet", "500000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("tezos-mainnet",     "tezos-mainnet:__native__",   NETWORK_NAME,  CRYPTO_NETWORK_CURRENCY_XTZ,  "native",   NULL,   true)
     DEFINE_UNIT ("tezos-mainnet:__native__",  "mutez",       "mtz",   0,  "mutez")
