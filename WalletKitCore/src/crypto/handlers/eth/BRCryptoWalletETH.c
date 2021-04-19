54727edc2671df5bf971b66a636778a8602886ae
Ed Gamble <ed@breadwallet.com>
2021-04-19 10:11:32 -0700

    CORE-1152: Get the Eth `nonce` correct on a new account

--- WalletKitCore/src/crypto/handlers/eth/BRCryptoWalletETH.c
+++ WalletKitCore/src/crypto/handlers/eth/BRCryptoWalletETH.c
@@ -276,40 +276,22 @@ cryptoWalletAnnounceTransferETH (BRCryptoWallet wallet,
     // We are only interested in updating the accounts nonce; therefore token wallets are ignored.
     if (NULL != walletETH->ethToken) return;
 
-    uint64_t nonce = 0;
+    uint64_t nonce = TRANSACTION_NONCE_IS_NOT_ASSIGNED;
 
     // Ignore `type`; just iterate over all transfers.
     for (size_t index = 0; index < array_count(wallet->transfers); index++) {
         BRCryptoTransferETH transferETH = cryptoTransferCoerceETH (wallet->transfers[index]);
 
-#if defined (NEVER_DEFINED)
-        switch (transferETH->base.direction) {
-            case CRYPTO_TRANSFER_RECEIVED:
-                recv +=1;
-                break;
-
-            case CRYPTO_TRANSFER_RECOVERED:
-                reco += 1;
-                goto updateNonce;
-
-            case CRYPTO_TRANSFER_SENT:
-                sent += 1;
-            updateNonce:
-                if (TRANSACTION_NONCE_IS_NOT_ASSIGNED != transferETH->nonce)
-                    nonce = MAX (nonce, transferETH->nonce);
-                break;
-        }
-#else
-
         if (CRYPTO_TRANSFER_RECEIVED != transferETH->base.direction &&
             TRANSACTION_NONCE_IS_NOT_ASSIGNED != transferETH->nonce)
-            nonce = MAX (nonce, transferETH->nonce);
-#endif
+            nonce = (TRANSACTION_NONCE_IS_NOT_ASSIGNED == nonce
+                     ? transferETH->nonce
+                     : MAX (nonce, transferETH->nonce));
     }
 
     ethAccountSetAddressNonce (walletETH->ethAccount,
                                ethAccountGetPrimaryAddress(walletETH->ethAccount),
-                               nonce + 1,
+                               (TRANSACTION_NONCE_IS_NOT_ASSIGNED == nonce ? 0 : (nonce + 1)),
                                ETHEREUM_BOOLEAN_TRUE);
 }
 
