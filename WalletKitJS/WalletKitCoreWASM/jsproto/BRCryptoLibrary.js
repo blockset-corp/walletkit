41e141bc8635c481b4150b8f8a9f5028b99ca0a3
Ed Gamble <ed@breadwallet.com>
2021-04-28 14:50:27 -0700

    CORE-1160: Remove WalletKitJS

--- WalletKitJS/WalletKitCoreWASM/jsproto/BRCryptoLibrary.js
+++
@@ -1,8 +0,0 @@
-var ref = require('ref');
-var ffi = require('ffi');
-
-var Core = ffi.Library('libCore', {
-  ...cryptoCurrencyDeclarations,
-  ...cryptoUnitDeclarations,
-  ...cryptoAccountDeclarations,
-})
