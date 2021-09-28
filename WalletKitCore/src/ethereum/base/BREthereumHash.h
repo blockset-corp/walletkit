11ab2c4f381823681c5f12820686729527e475bb
Ed Gamble <ed@breadwallet.com>
2021-09-28 08:36:00 -0700

    CORE-1283: BREthereum{Hash,Data} Additions

--- WalletKitCore/src/ethereum/base/BREthereumHash.h
+++ WalletKitCore/src/ethereum/base/BREthereumHash.h
@@ -73,6 +73,9 @@ ethHashCreateEmpty (void);
 extern BREthereumHash
 ethHashCreateFromData (BRRlpData data);
 
+extern BREthereumHash
+ethHashCreateFromBytes (const uint8_t *bytes, size_t bytesCount);
+
 /**
  * Return the hex-encoded string
  */
